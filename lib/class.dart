d533158fe951502aeedd2b5550319a09022f6e47
Daeseong Kim <daeseongkim05@gmail.com>
2024-10-31 11:40:50 +0900

    [Refactor] Change totalPrice logic

--- lib/class.dart
+++ lib/class.dart
@@ -7,14 +7,11 @@ class Product {
 
 class ShoppingMall {
   List<Product> productList;
-  int totalPrice;
+  int totalPrice = 0;
   Map<Product, int> cartList = {}; // 갯수 파악하기 위해 Map 사용
 
   // 초기화할때 상품 리스트를 받아 초기화
-  ShoppingMall(List<Product> products)
-      : productList = products,
-        totalPrice =
-            products.fold(0, (total, product) => total + product.price);
+  ShoppingMall(List<Product> products) : productList = products;
 
   void showProducts() {
     productList.map((product) => print("${product.name} / ${product.price}원"));
@@ -24,12 +21,11 @@ class ShoppingMall {
     cartList.update(product, (quantity) => quantity += 1);
   }
 
-  void showCartTotalPrice() {
-    int cartTotalPrice = 0;
+  void showTotal() {
     cartList.forEach((product, quentity) {
-      cartTotalPrice += product.price * quentity;
+      totalPrice += product.price * quentity;
       print("$product를 $quentity개 담으셨습니다.");
     });
-    print("장바구니에 $cartTotalPrice원 어치를 담으셨네요 !");
+    print("장바구니에 $totalPrice원 어치를 담으셨네요 !");
   }
 }
